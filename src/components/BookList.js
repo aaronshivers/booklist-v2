820eba1cf11e13b1802c3cf6fc0d64d9622a3bf7
aaronshivers <aaron@moonmail.org>
2019-08-04 07:34:36 -0500

    added populatebooks on page load

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,16 @@
-import React, { useContext } from 'react'
+import React, { useEffect, useContext } from 'react'
 import { Table } from 'react-bootstrap'
 import BooksContext from '../context/books-context'
 import DeleteButton from './DeleteButton'
 
 const BookList = () => {
   const { state, dispatch } = useContext(BooksContext)
+
+  // Populate books on page load
+  useEffect(() => {
+    const books = JSON.parse(localStorage.getItem('booklist'))
+    dispatch({ type: 'POPULATE_BOOKS', books })
+  }, [])
 
   return (
     <Table striped hover className="mt-5">
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,11 +1,13 @@
 export default (state, action) => {
   switch (action.type) {
     case 'ADD_BOOK':
-      const books = [ ...state, action.book ]
-      localStorage.setItem('booklist', JSON.stringify(books))
-      return books
+      const updatedBooks = [ ...state, action.book ]
+      localStorage.setItem('booklist', JSON.stringify(updatedBooks))
+      return updatedBooks
     case 'DELETE_BOOK':
       return state.filter(({ isbn }) => isbn !== action.isbn)
+    case 'POPULATE_BOOKS':
+      return action.books
     default:
       return state
   }
